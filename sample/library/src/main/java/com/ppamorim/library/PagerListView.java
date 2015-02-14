091a2e1689463cb4236e16222dbde6d3b3d25a4a
Pedro Paulo Amorim <pp.amorim@hotmail.com>
2015-02-14 20:26:02 -0200

    First commit

---
+++ sample/library/src/main/java/com/ppamorim/library/PagerListView.java
@@ -0,0 +1,26 @@
+package com.ppamorim.library;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by pedro on 2/14/15.
+ */
+public class PagerListView extends ListView {
+
+  public PagerListView(Context context) {
+    super(context);
+  }
+
+  public PagerListView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public PagerListView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+
+
+}
