2a66276a52d6285c0ce18270cce85e8add2fa5f3
Pedro Paulo Amorim <pp.amorim@hotmail.com>
2015-02-15 22:24:47 -0200

    Some corrections on PagerListView class

--- sample/library/src/main/java/com/ppamorim/library/PagerAdapter.java
+++ sample/library/src/main/java/com/ppamorim/library/PagerAdapter.java
@@ -1,3 +1,18 @@
+/*
+* Copyright (C) 2015 Pedro Paulo de Amorim.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.ppamorim.library;
 
 import android.content.Context;
@@ -8,6 +23,12 @@ import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import java.util.ArrayList;
 
+/**
+ * This is the base adapter for the PagerListView, this have 3 layouts and
+ * containerView is the principal view
+ *
+ * @author Pedro Paulo de Amorim.
+ */
 public class PagerAdapter extends BaseAdapter {
 
   private int mHeight;
--- sample/library/src/main/java/com/ppamorim/library/PagerListView.java
+++ sample/library/src/main/java/com/ppamorim/library/PagerListView.java
@@ -1,3 +1,18 @@
+/*
+* Copyright (C) 2015 Pedro Paulo de Amorim.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.ppamorim.library;
 
 import android.content.Context;
@@ -8,6 +23,12 @@ import android.view.View;
 import android.widget.ListView;
 import java.util.ArrayList;
 
+/**
+ * Custom ListView with alpha and parallax effect. When user click on next or back,
+ * the ListView will scroll on sexy moviment.
+ *
+ * @author Pedro Paulo de Amorim.
+ */
 public class PagerListView extends ListView {
 
   private Context mContext;
